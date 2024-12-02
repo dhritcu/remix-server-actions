d92eaa9fc53371bfe115221b32751935a6d18f86
Daniel Hritcu <134442484+dhritcu@users.noreply.github.com>
2024-12-02 17:00:21 -0500

    rpc: try loader args

--- app/modules/other-helllo.ts
+++ app/modules/other-helllo.ts
@@ -3,8 +3,10 @@ import { z } from "zod";
 
 export const sayOtherHello = procedure$("rpc.sayOtherHello")
   .input(z.object({ name: z.string() }))
-  .handler(async ({ input }) => {
-    console.debug("SERVER: sayOtherHello", input);
+  .handler(async ({ input, request }) => {
+    const url = new URL(request.url);
+    const path = url.pathname;
+    console.debug("SERVER: sayOtherHello", input, path);
     return {
       message: `Hi ${input.name}!`,
     };
